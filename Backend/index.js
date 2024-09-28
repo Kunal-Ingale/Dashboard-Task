382185d41efc58748bb28ac5f61d8faaad1d2be7
Kunal Ingale <112793345+Kunal-Ingale@users.noreply.github.com>
2024-09-28 22:11:38 +0530

    Update cors

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin:[""],
+        origin:["https://dekstop-task-api.vercel.app/"],
         methods:["GET"],
         credentials:true
     }
