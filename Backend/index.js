e649abcfe92a0ccba694caccd4b4b0d288bd2e4b
Kunal Ingale <112793345+Kunal-Ingale@users.noreply.github.com>
2024-09-28 22:15:50 +0530

    Update URL

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin:[""],
+        origin:["https://dekstop-task-frontend.vercel.app/"],
         methods:["GET"],
         credentials:true
     }
