845066967f318bdfefa5e2ac6d13db555f405dcb
Kunal Ingale <112793345+Kunal-Ingale@users.noreply.github.com>
2024-09-30 18:28:05 +0530

    Update axiosInstance.jsx

--- Frontend/Hooks/axiosInstance.jsx
+++ Frontend/Hooks/axiosInstance.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://dekstop-task-api.vercel.app/api/transactions', 
+  baseURL: 'https://dashboard-task-api.vercel.app//api/transactions', 
 });
 
 export default axiosInstance;
