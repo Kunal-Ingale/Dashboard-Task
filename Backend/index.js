3a6ca14fcd1a774161566424b38e1657dbaf6402
Kunal Ingale <112793345+Kunal-Ingale@users.noreply.github.com>
2024-09-30 18:49:01 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin:["https://dekstop-task-frontend.vercel.app"],
+        origin:["https://dashboard-self-sigma-37.vercel.app"],
         methods:["GET","POST", "PUT", "DELETE"],
         credentials:true
     }
